bf5d881bb4ca4fc58c3c4a87cb245cee9ca4101d
zzombay <zzoombay@gmail.com>
2024-04-12 19:35:46 +0400

    update

--- Client/cmake-build-debug/cmake_install.cmake
+++ Client/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/project_git/Client-Server
+# Install script for directory: D:/project_git/Client-Server/Client
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/project_git/Client-Server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/project_git/Client-Server/Client/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
